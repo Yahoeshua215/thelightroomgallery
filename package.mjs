cd2e250e8679ab71c5d14a67375a9049887cceda
Marowitz <jmarowitz@onesignal.com>
2026-09-23 21:03:30 -0400

    Replace the header logo with the archival LightRoom Gallery wordmark.

--- package.mjs
+++ package.mjs
@@ -6,7 +6,7 @@ const dataUri = (filename) => {
   return `data:image/png;base64,${image.toString('base64')}`;
 };
 const poster = dataUri('in-good-company-poster.png');
-const logo = dataUri('lightroom-gallery-logo-v2.png');
-const standalone = html.replaceAll('href="in-good-company-poster.png"', `href="${poster}"`).replace('src="in-good-company-poster.png"', `src="${poster}"`).replace('src="lightroom-gallery-logo-v2.png"', `src="${logo}"`).replace(' download>', ' download="in-good-company-poster.png">');
+const logo = dataUri('lightroom-gallery-logo.png');
+const standalone = html.replaceAll('href="in-good-company-poster.png"', `href="${poster}"`).replace('src="in-good-company-poster.png"', `src="${poster}"`).replace('src="lightroom-gallery-logo.png"', `src="${logo}"`).replace(' download>', ' download="in-good-company-poster.png">');
 fs.writeFileSync(new URL('in-good-company-standalone.html', root), standalone);
 console.log('Created standalone HTML with embedded artwork.');
